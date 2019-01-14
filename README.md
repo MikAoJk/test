e446dcb89ba1b857c2fac34807193888ab986fad
Joakim kartveit <joakim.kartveit@nav.no>
2019-01-14 13:23:53 +0100

    La til minimum får å kjøre ein java applikasjon med docker

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# test
+# test applikasjon
+This for testing testing Kotlin DSL and Docker
+
+## Technologies used
+* Kotlin
+* Gradle
+
+## Getting started
+## Running locally
+
+### Building the application
+#### Compile and package application
+To build locally and run the integration tests you can simply run `./gradlew shadowJar` or  on windows 
+`gradlew.bat shadowJar`
+
+#### Creating a docker image
+Creating a docker image should be as simple as `docker build -t test-applikasjon .`
+
+## Contact us
+### Code/project related questions can be sent to
+* Joakim Kartveit, `joakim.kartveit@gmail.com`
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,38 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.10"
+    id("com.github.johnrengelman.shadow") version "4.0.3"
 }
 
 group = "no.kartveit"
 version = "1.0-SNAPSHOT"
+
+
+task("fatJar", type = Jar::class) {
+    manifest {
+        attributes["Main-Class"] = "no.kartveit.BootstrapKt"
+    }
+}
+
+tasks.withType<Jar>{
+    manifest {
+        attributes(mapOf(
+            "Main-Class" to "no.kartveit.BootstrapKt"
+        ))
+    }
+}
+
+tasks.withType<ShadowJar> {
+    manifest {
+        attributes(mapOf(
+            "Main-Class" to "no.kartveit.BootstrapKt"
+        ))
+    }
+    mergeServiceFiles()
+}
+
 
 repositories {
     mavenCentral()
